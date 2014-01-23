82032d9734e58cc4111c7aec14b0250b142a28d5
Jared Stemen <jstemen@gmail.com>
2014-01-22 22:50:33 -0500

    A couple of polishing touches.  Added instructions on how to run from the command line.  Disabled broken Jersey-Spring setup.

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ json-to-db
 ==========
 
 A simple program that reads a text file containing an array JSON product objects and loads then into a database where they are accessible via a REST endpoint.
+
+Compile the program and import the data into the db with:
+mvn install exec:java -Dexec.mainClass=com.jaredstemen.blogspot.jsonimport.JsonFileImporterRunner
+
+Start up the web server with:
+mvn jetty:run
--- src/main/java/com/jaredstemen/blogspot/jsonimport/JsonFileImporterRunner.java
+++ src/main/java/com/jaredstemen/blogspot/jsonimport/JsonFileImporterRunner.java
@@ -11,7 +11,7 @@ import java.text.ParseException;
  * Run this class to extract data from the JSON file and put it in the db.
  */
 public class JsonFileImporterRunner {
-    public static void main() throws IOException, ParseException {
+    public static void main(String[] args) throws IOException, ParseException {
         ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
 
         JsonFileImporter jsonFileImporter = ctx.getBean(JsonFileImporter.class);
